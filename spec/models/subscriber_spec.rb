d41ec02f18c1e5d29bcea05587eed5e37b6d58cc
AileenBull <aileen.cacayorin@gmail.com>
2022-08-24 22:05:01 -0500

    add model spec

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Subscriber do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should_not allow_value('invalid_email_format').for(:email) }
 end
